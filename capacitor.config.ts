fe5813a9e96d21e4590f51cccf5598a70f39f860
Alex Guerrero <alex.guerrero@sngular.com>
2024-04-12 14:23:08 +0200

    feat: add vr

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-app',
+  webDir: 'dist/wwww',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
